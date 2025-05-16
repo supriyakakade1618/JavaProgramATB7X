313e20960cefa7ffb3ac3f6b0b98d33ad3fb758a
admin <supriyakakade1618@gmail.com>
2025-05-16 18:56:01 +0530

    new files added

---
+++ src/Ex_15052025/Lab0.java
@@ -0,0 +1,15 @@
+package Ex_15052025;
+
+public class Lab0 {
+    public static void main(String[] args) {
+        int a=100;
+        if (a>13)
+        {
+            System.out.println("a is greater");
+        }
+        else
+        {
+            System.out.println("a is not greater");
+        }
+    }
+}
